0017cf84628c32a0dd844a94e84566b2d7fd1d16
veggis <vegard.schau@knowit.no>
2021-10-29 00:04:06 +0200

    Added props to CustomGeoJson

--- src/components/CustomGeoJson.tsx
+++ src/components/CustomGeoJson.tsx
@@ -3,7 +3,10 @@ import { Data, getAvgCases, getColor } from '../utils/Util';
 import { GeoJSON, Tooltip } from 'react-leaflet';
 import '../css/table.css';
 
-const CustomGeoJson: FC = () => {
+const CustomGeoJson: FC<{
+    item: Data;
+    onClick: () => void;
+}> = ({ item, onClick }) => {
     return null;
 };
 
